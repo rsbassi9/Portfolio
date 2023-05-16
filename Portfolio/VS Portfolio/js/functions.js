ec2eb95fb2d0720489e3988867e0e00e1a917857
rsbassi9 <rajvir.bassi95@gmail.com>
2023-05-15 22:13:37 -0700

    changed locations of files

--- Portfolio/VS Portfolio/js/functions.js
+++
@@ -1,9 +0,0 @@
-// Get the burger menu button and navigation menu
-const burgerMenu = document.getElementById('burger-menu');
-const navbar = document.getElementById('navbar');
-
-// Add an event listener to the burger menu button
-burgerMenu.addEventListener('click', () => {
-  // Toggle the show class on the navigation menu
-  navbar.classList.toggle('show');
-});
